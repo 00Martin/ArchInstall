8d661ee104efc73cf5aa4095767def7e01c7b47c
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-08-05 22:18:49 +0200

    installation of additional packages

--- install3.sh
+++ install3.sh
@@ -29,6 +29,59 @@ rm -rf yay
 sudo localectl set-x11-keymap ch "" fr
 
 
+#We ask the user if they would like to install additionnal programs
+echo -e "\n\n\nWould you like to install additionnal programs? (You will be asked for each individually) [n/Y]"
+read answerPrograms
+#If we do want to install them
+if [ $answerPrograms == "Y" ]; then
+#Yes I could've done this section cleaner with a loop and list but the extra work and testing was not worth the time for a few programs
+#Potential improvements to make
+# - a table containing the name and type (pacman=0, aur=1) of the package
+# - a loop that goes through each of them and asks the user, as well as an if that checks if it has to be installed with pacman or yay
+    echo -e "\n\nWould you like to install Brave Browser? [n/Y]"
+    read answerBrave
+    if [ $answerBrave == "Y" ]; then
+    yay -Sy --noconfirm brave-bin
+    fi
+
+    echo -e "\n\nWould you like to install Firefox? [n/Y]"
+    read answerFirefox
+    if [ $answerFirefox == "Y" ]; then
+    sudo pacman -Sy --noconfirm firefox
+    fi
+
+    echo -e "\n\nWould you like to install Discord? [n/Y]"
+    read answerDiscord
+    if [ $answerDiscord == "Y" ]; then
+    sudo pacman -Sy --noconfirm discord
+    fi
+
+    echo -e "\n\nWould you like to install Spotify? [n/Y]"
+    read answerSpotify
+    if [ $answerSpotify == "Y" ]; then
+    yay -Sy --noconfirm spotify
+    fi
+
+    echo -e "\n\nWould you like to install Minecraft? [n/Y]"
+    read answerMinecraft
+    if [ $answerMinecraft == "Y" ]; then
+    yay -Sy --noconfirm minecraft-launcher
+    fi
+
+    echo -e "\n\nWould you like to install Fastfetch? [n/Y]"
+    read answerFastfetch
+    if [ $answerFastfetch == "Y" ]; then
+    yay -Sy --noconfirm fastfetch
+    fi
+
+    echo -e "\n\nWould you like to install Vlc? [n/Y]"
+    read answerVlc
+    if [ $answerVlc == "Y" ]; then
+    sudo pacman -Sy --noconfirm vlc
+    fi
+fi
+
+
 #We ask the user if they would like to set a custom DNS server
 echo -e "\n\n\nWould you like to set a custom IPv4 DNS server? [n/Y] (The system will reboot after this step)"
 read answer
