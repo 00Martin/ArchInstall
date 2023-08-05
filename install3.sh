31dc7f3598d18302b4550c121c9f37765cf6cdb5
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-08-05 17:35:05 +0200

    make the installation of gaming packages optional

--- install3.sh
+++ install3.sh
@@ -3,8 +3,17 @@
 #file: install3.sh
 #v1.0 - 04.15.2023 - latest change: initial release
 
-#We install some useful (and gaming) packages
-sudo pacman -Sy --noconfirm steam ttf-liberation git base-devel flatpak wine-staging lutris
+#We install some useful packages
+sudo pacman -Sy --noconfirm ttf-liberation git base-devel flatpak
+
+#We ask the user if they would like to install some gaming packages
+echo -e "\n\n\nWould you like to install the following gaming packages? (Steam, Lutris) [n/Y]"
+read answerGaming
+#If we do want to install them
+if [ $answerGaming == "Y" ]; then
+#We install the gaming packages
+sudo pacman -Sy --noconfirm steam wine-staging lutris
+fi
 
 
 #Installation of Yay (AUR Helper)
@@ -21,7 +30,7 @@ sudo localectl set-x11-keymap ch "" fr
 
 
 #We ask the user if they would like to set a custom DNS server
-echo -e "\n\n\nWould you like to set a custom IPv4 DNS server ? [n/Y] (The system will reboot after this step)"
+echo -e "\n\n\nWould you like to set a custom IPv4 DNS server? [n/Y] (The system will reboot after this step)"
 read answer
 
 #If we do want to set a custom DNS
