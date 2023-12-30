0ed94d3a10b52e6afd94ad17a3f115785d831802
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-12-30 22:50:53 +0100

    Enabling the possibility of ssd trims on an encrypted system

--- install1.sh
+++ install1.sh
@@ -155,11 +155,39 @@ echo "linux /vmlinuz-linux"         >>  /mnt/boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img"  >>  /mnt/boot/loader/entries/arch.conf
 
 if [[ $doWeEncrypt == "1" ]]; then
-    #If we are encrypted, we need to add LUKS specific options
-    encryptedUUID=$(blkid -s UUID -o value /dev/$partitionNameRoot)
-    echo "options rd.luks.name=$encryptedUUID=root root=/dev/mapper/root"    >>  /mnt/boot/loader/entries/arch.conf
+    #If we are encrypted, we need to add LUKS specific kernel parameters to be able to boot on that encrypted drive
+    #If the encrypted device is an SSD, we should enable regular trimming as well, this is done by using the luks.options=discard parameter to allow trimming, then enabling the systemd service fstrim which will do the trimming (done in the 2nd script).
+    #Trimming is disabled by default on ext4, but we already set up the "infrastructure" to be able to quickly configure trimming in the future
+    #In this setup our partitions are built by layers (First encrypted LUKS, then LVM, then ext4), LUKS will have discarding (trimming) enabled, LVM passes the discard request automatically, then ext4 is left to be configured.
+    #Trimming an encrypted drive "leaks" information about which blocks are used and which are free, but here I'm looking for a middle ground between performance and absolute security.
+    #If the device is not encrypted, nothing needs to be done because in that case we use btrfs which automatically enables asynchronous trimming if the drive is capable of TRIMs
+    #We ask the user if they have a TRIM capable SSD
+    echo -e "\n\nIs your primary storage device a TRIM capable SSD? (Answering wrong to this question can have consequences like data loss or reduced SSD longevity) [n/Y]"
+    read answerTrim
 
-    #There's some work the user needs to do, we give them the instructions
+    #We add an additional kernel parameter if the SSD is TRIM capable
+    if [[ $answerTrim == "Y" ]]; then
+
+        #Trim Capability
+        shouldWeTrim="1"
+
+        encryptedUUID=$(blkid -s UUID -o value /dev/$partitionNameRoot)
+        echo "options rd.luks.name=$encryptedUUID=root root=/dev/mapper/root rd.luks.options=discard"    >>  /mnt/boot/loader/entries/arch.conf
+
+    else
+
+        #Trim Capability
+        shouldWeTrim="0"
+
+        encryptedUUID=$(blkid -s UUID -o value /dev/$partitionNameRoot)
+        echo "options rd.luks.name=$encryptedUUID=root root=/dev/mapper/root"    >>  /mnt/boot/loader/entries/arch.conf
+
+    fi
+
+    #We save the user's SSD' trimming capability choice onto a file
+    echo "$shouldWeTrim" > /mnt/home/ssdTrim.doNotDelete
+
+    #Finally, there's some work the user needs to do, we give them the instructions
     echo -e "\n\n\nTo make your encrypted partition accessible, there's some extra work to do, don't worry, it's not gonna take long."
     echo -e "\nTake a picture of this before proceeding, the instructions will disappear."
     echo -e "\n\nFirst, we need to edit the HOOKS of the mkinitcpio.conf file."
