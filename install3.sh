af832205b6e82fb0a96ad8a3dbe659862393ae84
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2024-06-09 02:45:29 +0200

    Scrapped custom DNS configuration

--- install3.sh
+++ install3.sh
@@ -76,21 +76,6 @@ if [[ $answerPrograms == "Y" ]]; then
 fi
 
 
-#We ask the user if they would like to set a custom DNS server
-echo -e "\n\n\nWould you like to set a custom IPv4 DNS server? [n/Y] (The system will reboot after this step)"
-read answer
-
-#If we do want to set a custom DNS
-if [[ $answer == "Y" ]]; then
-echo -e "\n\nEnter the IP of the DNS server (format x.x.x.x)\nThis script does not verify if the IP format is valid, so make sure to enter it properly."
-read customdns
-
-#We add with sudo privileges the static DNS server to the dhcpcd config file
-sudo sh -c "echo 'static domain_name_servers=$customdns' >> /etc/dhcpcd.conf"
-
-fi
-
-
 #Auto clean, we remove the scripts
 sudo rm /home/install2.sh
 sudo rm /home/install3.sh
